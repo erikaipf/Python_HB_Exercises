50d3c6dd8c77dcf17f91d19a0d0682e3c907fff3
Erika Iamamoto Pacheco <Erika Iamamoto Pacheco>
2016-05-03 15:12:06 -0700

    examples of code

---
+++ date_time.py
@@ -0,0 +1,10 @@
+from datetime import date
+hj = date.today()
+
+print hj
+
+print hj.day
+
+print hj.month
+
+print hj.year
---
+++ greater_name.py
@@ -0,0 +1,10 @@
+my_name = raw_input("What is your name?")
+
+my_partners_name = raw_input("What is your pair's name?")
+
+if my_name > my_partners_name:
+   	print 'My name is greater'
+elif my_name < my_partners_name:
+   	print 'Their name is greater'
+else:
+	print 'our names are equal!'
